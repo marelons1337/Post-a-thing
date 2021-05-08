f42421cc08a890134635ab5bb652bda774f13750
rafal <jarorafo@gmail.com>
2021-05-08 10:56:04 +0200

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    public function index() {
+    
+        $comments = Comment::orderByDesc('created_at')->limit(3)->get();
+
+        return view('comments/index', ['comments' => $comments]);
+    }
+
+    public function show($id) {
+
+        $user = User::findOrFail($id);
+        return view('comments/show', ['user' => $user]);
+    }
+
+    public function create () {
+        return view('comments/create'); 
+    }
+
+    public function store() {
+        $comment = new Comment();
+         
+        $comment->username = "test";
+        $comment->comment = request('comment');
+        $comment->urgency = request('urgency');
+        $comment->contact_methods = request('contact');
+
+        $comment->save();
+
+        return redirect('/')->with('mssg', 'Your thing has been posted!');
+    }
+
+    public function destroy($id) {
+        $user = User::findOrFail($id); 
+        $user->delete();
+
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [UserController::class, 'index']);
+
+Route::post('/', [UserController::class, 'store']);
+
+Route::get('//create', [UserController::class, 'create']);
+
+Route::get('//{user}', [UserController::class, 'show']);
+
+Route::delete('/{id}', [UserController::class, 'destroy']);
+
