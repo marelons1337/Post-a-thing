0b94e7d91f379442203bf043147515c9ae6b7252
rafal <jarorafo@gmail.com>
2021-05-21 19:45:27 +0200

    show view styling

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <div class="content">
-        <h1>Post by <a href={{ route('users.show', $comment->user_id) }}>{{ $comment->username }}</a></h1><br />
-        <p class="small">{{ $comment->created_at }}, Urgency {{ $comment->urgency }}</p>
+<div class="show-container">
+    <div class="show-content">
+        <h1 class="show-header">Post by <a href={{ route('users.show', $comment->user_id) }}>{{ $comment->username }}</a></h1><br />
+        <p>{{ $comment->created_at }}, Urgency {{ $comment->urgency }}</p>
         <p>{{ $comment->comment }} </p>
         <a href="/" class="back"><- Go back</a>
         <form action="{{ route('comments.destroy', $comment->id) }}" method="POST">
             @csrf
             @method('DELETE')
-            <button>Delete Comment</button>
+            <button class="delete-comment-button">Delete Comment</button>
         </form>
     </div>
  </div>
